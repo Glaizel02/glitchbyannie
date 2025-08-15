3faa9def0f038003a08b54608073a0195e9f7cae
Glaizel02 <zelglai64@gmail.com>
2025-08-15 16:01:32 +0800

    Update glitch.py

--- glitch.py
+++ glitch.py
@@ -1,5 +1,5 @@
 # Hidden Code Executor
-# This file contains a hidden version of annixxy
+# DONT TRY TO MODIFIED BITCH!
 
 import base64
 _ = base64.b64decode("""IyBTaW1wbGUgcGxhY2Vob2xkZXIgY29kZSB0byBkZW1vbnN0cmF0ZSBvYmZ1c2NhdGlvbgppbXBvcnQgc3lzCgpwcmludCgiSGVsbG8sIHRoaXMgaXMgbGFiLnB5IHJ1bm5pbmcgb2JmdXNjYXRlZC4iKQpwcmludChmIlN5cyB2ZXJzaW9uOiB7c3lzLnZlcnNpb259Iik=""")
